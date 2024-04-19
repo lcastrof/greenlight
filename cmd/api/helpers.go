4abf99f0c86ea11a2b3545f8200b394074ee1ed4
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-18 23:29:06 -0300

    feat(3.4.2): create helper to envelope json responses

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -23,33 +23,25 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-// Define a writeJSON() helper for sending responses. This takes the destination
-// http.ResponseWriter, the HTTP status code to send, the data to encode to JSON, and a
-// header map containing any additional HTTP headers we want to include in the response.
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
-	// Use the json.MarshalIndent() function so that the whitespace is added to the encoded
-	// JSON. Here we use no line prefix ("") and tab indents("\t") for each element.
-	// This costs considerably more than Marshal itself, so if the app runs on a
-	// very strict env or there are lots of requests per second, it should be better
-	// sticking with Marshal
+// Define an envelope type.
+type envelope map[string]interface{}
+
+// Change the data parameter to have the type envelope instead of interface{}.
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
 	}
-	// Append a newline to make it easier to view in terminal applications.
+
 	js = append(js, '\n')
-	// At this point, we know that we won't encounter any more errors before writing the
-	// response, so it's safe to add any headers that we want to include. We loop
-	// through the header map and add each header to the http.ResponseWriter header map.
-	// Note that it's OK if the provided header map is nil. Go doesn't throw an error
-	// if you try to range over (or generally, read from) a nil map.
+
 	for key, value := range headers {
 		w.Header()[key] = value
 	}
-	// Add the "Content-Type: application/json" header, then write the status code and
-	// JSON response.
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
 	w.Write(js)
+
 	return nil
 }

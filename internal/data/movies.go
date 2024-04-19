3dcde5485847a4e0645d542f243e4022abd685db
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-18 22:37:12 -0300

    feat(3.3.2): add struct tags to improve json output

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,18 +4,14 @@ import (
 	"time"
 )
 
-// Important: It’s crucial to point out here that all the fields in our Movie
-// struct are exported (i.e. start with a capital letter), which is necessary
-// for them to be visible to Go’s encoding/json package.
-// Any fields which aren’t exported won’t be included when encoding a
-// struct to JSON.
+// Annotate the Movie struct with struct tags to control how the keys appear
+// in the JSON-encoded output
 type Movie struct {
-	ID        int64     // Unique integer ID for the movie
-	CreatedAt time.Time // Timestamp for when the movie is added to our database
-	Title     string    // Movie title
-	Year      int32     // Movie release year
-	Runtime   int32     // Movie runtime(in minutes)
-	Genres    []string  // Slice of genres for the movie
-	Version   int32     // The version number starts at 1 and will be incremented each
-	// time the movie information is updated
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Title     string    `json:"title"`
+	Year      int32     `json:"year"`
+	Runtime   int32     `json:"runtime"`
+	Genres    []string  `json:"genres"`
+	Version   int32     `json:"version"`
 }

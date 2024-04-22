96c66d63404bed12acc8cfc00d706ce441afadc5
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-22 15:31:51 -0300

    feat(7.1): setup movie database model

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -10,19 +10,12 @@ import (
 	"os"
 	"time"
 
-	// Import the pq driver so that it can register itself with the database/sql
-	// package. Note that we alias this import to the blank identifier, to stop the Go
-	// compiler complaining that the package isn't being used.
+	"github.com/lcastrof/greenlight/internal/data"
 	_ "github.com/lib/pq"
 )
 
-// Declare a string containing the application version number. Later in the book
-// we'll generate this automatically at the build time, but for now we'll just
-// store the version number as a hard-coded global constant.
 const version = "1.0.0"
 
-// Add maxOpenConns, maxIdleConns and maxIdleTime fields to hold the configuration
-// settings for the connection pool.
 type config struct {
 	port int
 	env  string
@@ -34,12 +27,10 @@ type config struct {
 	}
 }
 
-// Define the application struct to hold the dependencies for our HTTP handlers,
-// helpers and middleware. At the moment, this only contains a copy of the config
-// struct and a logger, but it will grow to include a lot more as our build progresses.
 type application struct {
 	config config
 	logger *slog.Logger
+	models data.Models
 }
 
 func main() {
@@ -48,14 +39,8 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 4000, "Api server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
 
-	// Read the DSN value from the db-dsn command-line flag into the config struct. We
-	// default to using our development DSN if no flag is provided.
-	// Use the value of the GREENLIGHT_DB_DSN environment variable as the default value
-	// for our db-dsn command-line flag.
 	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 
-	// Read the connection pool settings from command-line flags into the config struct.
-	// Notice the default values that we're using?
 	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
 	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
 	flag.StringVar(&cfg.db.maxIdleTime, "db-max-idle-time", "15m", "PostgreSQL max connection idle time")
@@ -64,25 +49,21 @@ func main() {
 
 	logger := slog.Default()
 
-	// Call the openDB() helper function (see below) to create the connection pool,
-	// passing in the config struct. If this returns an error, we log it and exit the
-	// application immediately.
 	db, err := openDB(cfg)
 	if err != nil {
 		logger.Error(err.Error())
 	}
 
-	// Defer a call to db.Close() so that the connection pool is closed before the
-	// main() function exits.
 	defer db.Close()
 
-	// Also log a message to say that the connection pool has been successfully
-	// established.
 	logger.Info("database connection pool established")
 
+	// Use the data.NewModels() function to initialize a Models struct, passing in the
+	// connection pool as a parameter.
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: data.NewModels(db),
 	}
 
 	srv := &http.Server{
@@ -94,35 +75,25 @@ func main() {
 	}
 
 	logger.Info(fmt.Sprintf("Starting %s server on %s", cfg.env, srv.Addr))
-	// Because the err variable is now already declared in the code above, we need
-	// to use the = operator here, instead of the := operator.
 	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 }
 
-// The openDB() function returns a sql.DB connection pool.
 func openDB(cfg config) (*sql.DB, error) {
 	db, err := sql.Open("postgres", cfg.db.dsn)
 	if err != nil {
 		return nil, err
 	}
 
-	// Set the maximum number of open (in-use + idle) connections in the pool. Note that
-	// passing a value less than or equal to 0 will mean there is no limit.
 	db.SetMaxOpenConns(cfg.db.maxOpenConns)
 
-	// Set the maximum number of idle connections in the pool. Again, passing a value
-	// less than or equal to 0 will mean there is no limit.
 	db.SetMaxIdleConns(cfg.db.maxIdleConns)
 
-	// Use the time.ParseDuration() function to convert the idle timeout duration string
-	// to a time.Duration type.
 	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
 	if err != nil {
 		return nil, err
 	}
 
-	// Set the maximum idle timeout.
 	db.SetConnMaxIdleTime(duration)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,25 +1,20 @@
 package data
 
 import (
+	"database/sql"
 	"time"
 
 	"github.com/lcastrof/greenlight/internal/validator"
 )
 
-// Annotate the Movie struct with struct tags to control how the keys appear
-// in the JSON-encoded output
 type Movie struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"-"` // Use the - directive to omit field from output
 	Title     string    `json:"title"`
-	Year      int32     `json:"year,omitempty"` // Add the omitempty directive to hide the field from output IF it is empty
-	// Use the Runtime type instead of int32. Note that the omitempty directive will
-	// still work on this: if the Runtime field has the underlying value 0, then it will
-	// be considered empty and omitted -- and the MarshalJSON() method we just made
-	// won't be called at all.
-	Runtime Runtime  `json:"runtime,omitempty"` // Add the omitempty directive
-	Genres  []string `json:"genres,omitempty"`  // Add the omitempty directive
-	Version int32    `json:"version"`
+	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive to hide the field from output IF it is empty
+	Runtime   Runtime   `json:"runtime,omitempty"` // Add the omitempty directive
+	Genres    []string  `json:"genres,omitempty"`  // Add the omitempty directive
+	Version   int32     `json:"version"`
 }
 
 func ValidateMovie(v *validator.Validator, movie *Movie) {
@@ -37,4 +32,29 @@ func ValidateMovie(v *validator.Validator, movie *Movie) {
 	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
 	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
 	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+}
+
+// Define a MovieModel struct type which wraps a sql.DB connection pool.
+type MovieModel struct {
+	DB *sql.DB
+}
+
+// Add a placeholder method for inserting a new record in the movies table.
+func (m MovieModel) Insert(movie *Movie) error {
+	return nil
+}
+
+// Add a placeholder method for fetching a specific record from the movies table.
+func (m MovieModel) Get(id int64) (*Movie, error) {
+	return nil, nil
+}
+
+// Add a placeholder method for updating a specific record in the movies table.
+func (m MovieModel) Update(movie *Movie) error {
+	return nil
+}
+
+// Add a placeholder method for deleting a specific record from the movies table.
+func (m MovieModel) Delete(id int64) error {
+	return nil
 }

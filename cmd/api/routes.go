bed449c932587569c971297b69f64e78988a7270
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-23 23:09:09 -0300

    feat(9.2): add listMovies route with query string parsing

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -18,10 +18,11 @@ func (app *application) routes() *httprouter.Router {
 	// and set it as the custom error handler for 405 Method Not Allowed responses.
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
+	// Add the route for the GET /v1/movies endpoint.
+	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
-	// Require a PATCH request, rather than PUT.
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
 

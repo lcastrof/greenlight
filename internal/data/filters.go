28f59cd9b24e7d2563dc945427f346a9f52b3860
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-28 20:23:47 -0300

    feat(9.7): add pagination to list route

--- internal/data/filters.go
+++ internal/data/filters.go
@@ -37,6 +37,20 @@ func (f Filters) sortDirection() string {
 	return "ASC"
 }
 
+func (f Filters) limit() int {
+	return f.PageSize
+}
+
+// In the offset() method there is the theoretical risk of an integer overflow
+// as we are multiplying two int values together. However, this is mitigated by
+// the validation rules we created in our ValidateFilters() function, where we
+// enforced maximum values of page_size=100 and page=10000000 (10 million).
+// This means that the value returned by offset() should never come close
+// to overflowing.
+func (f Filters) offset() int {
+	return (f.Page - 1) * f.PageSize
+}
+
 func ValidateFilters(v *validator.Validator, f Filters) {
 	// Check that the page and page_size parameters contain sensible values.
 	v.Check(f.Page > 0, "page", "must be greater than zero")

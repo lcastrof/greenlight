31edfebe8c03618223ccbd6c111c9a4daa1b3dc2
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-29 16:58:33 -0300

    feat(13.2): set up users model

--- internal/data/models.go
+++ internal/data/models.go
@@ -10,25 +10,15 @@ var (
 	ErrEditConflict   = errors.New("edit conflict")
 )
 
-// Create a Models struct which wraps the MovieModel. We'll add other models to this,
-// like a UserModel and PermissionModel, as our build progresses.
 type Models struct {
-	// Set the Movies field to be an interface containing the methods that both the
-	// 'real' model and mock model need to support.
-	Movies interface {
-		Insert(movie *Movie) error
-		Get(id int64) (*Movie, error)
-		Update(movie *Movie) error
-		Delete(id int64) error
-		GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
-	}
+	Movies MovieModel
+	Users  UserModel // Add a new Users field.
 }
 
-// For ease of use, we also add a New() method which returns a Models struct containing
-// the initialized MovieModel.
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db}, // Initialize a new UserModel instance.
 	}
 }
 

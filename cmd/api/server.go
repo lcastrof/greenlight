b278d5002b34456e448ed5846741739f222a8241
Lucas de Castro <lucascastrofr@gmail.com>
2024-04-28 23:46:17 -0300

    feat(12.2.2): intercept shutdown signals

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func (app *application) serve() error {
-	// Declare a HTTP server using the same settings as in our main() function.
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
@@ -16,9 +18,31 @@ func (app *application) serve() error {
 		WriteTimeout: 30 * time.Second,
 	}
 
-	// Likewise log a "starting server" message.
+	// Start a background goroutine.
+	go func() {
+		// Create a quit channel which carries os.Signal values.
+		quit := make(chan os.Signal, 1)
+
+		// Use signal.Notify() to listen for incoming SIGINT and SIGTERM signals and
+		// relay them to the quit channel. Any other signals will not be caught by
+		// signal.Notify() and will retain their default behavior.
+		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+		// Read the signal from the quit channel. This code will block until a signal is
+		// received.
+		s := <-quit
+
+		// Log a message to say that the signal has been caught. Notice that we also
+		// call the String() method on the signal to get the signal name and include it
+		// in the log entry properties.
+		app.logger.Info("caught signal", "signal", s.String())
+
+		// Exit the application with a 0 (success) status code.
+		os.Exit(0)
+	}()
+
+	// Start the server as normal.
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
 
-	// Start the server as normal, returning any error.
 	return srv.ListenAndServe()
 }
